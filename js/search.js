563d09e1c8926d4fdcd318ad2234bf605334a7b3
remexre <nathan@remexre.xyz>
2019-12-14 07:38:19 +0000

    Deploying to master from authoring 3c492d2a09fae26299dbd2a7bcc8393c2e0c8cce

---
+++ js/search.js
@@ -0,0 +1,35 @@
+(function() {
+  // TODO: Are IIFEs still best practice?
+
+  // Add the input element. This way, the feature only exists when it's supported!
+  const searchBox = document.createElement("input");
+  searchBox.autocomplete = "off";
+  searchBox.id = "search";
+  searchBox.placeholder = "Search...";
+  document.querySelector("#pages").prepend(searchBox);
+
+  // Add search functionality.
+  const normalize = (s) => {
+    return s.toLowerCase().replace(/[ \(\)]/g, "");
+  };
+  const updateSearch = () => {
+    const searchTerm = normalize(searchBox.value);
+    const pages = document.querySelectorAll(".page");
+    for(let i = 0; i < pages.length; i++) {
+      const classes = pages[i].classList;
+      const normalized = normalize(pages[i].textContent);
+      const show = normalized.search(searchTerm) >= 0;
+      const hidden = classes.contains("hidden");
+      if(show && hidden)
+        classes.remove("hidden");
+      else if(!show && !hidden)
+        classes.add("hidden");
+    }
+  };
+  searchBox.addEventListener("keydown", () => {
+    window.setTimeout(updateSearch, 1);
+  });
+  searchBox.addEventListener("keypress", () => {
+    window.setTimeout(updateSearch, 1);
+  });
+})();
