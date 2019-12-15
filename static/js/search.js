1d074d6cccf69614910f86d5d1212b47ff14d726
Nathan Ringo <nathan@remexre.xyz>
2019-12-15 02:41:31 -0600

    ...

--- static/js/search.js
+++ static/js/search.js
@@ -1,12 +1,34 @@
+// MIT License
+// 
+// Copyright (c) 2019 Nathan Ringo
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to
+// deal in the Software without restriction, including without limitation the
+// rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+// sell copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+// IN THE SOFTWARE.
+
 (function() {
   // TODO: Are IIFEs still best practice?
 
   // Add the input element. This way, the feature only exists when it's supported!
   const searchBox = document.createElement("input");
   searchBox.autocomplete = "off";
-  searchBox.id = "search";
+  searchBox.classList.add("search");
   searchBox.placeholder = "Search...";
-  document.querySelector("#pages").prepend(searchBox);
+  document.querySelector(".pages").prepend(searchBox);
 
   // Add search functionality.
   const normalize = (s) => {
